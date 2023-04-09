d2915fa2965331d3c7b58f3f5125f15e0683686d
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-04-09 10:58:30 +0200

    FaceSnapsService: modify data snapface book

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -8,52 +8,52 @@ export class FaceSnapsService {
   faceSnaps: FaceSnap[] = [
     {
       id: 1,
-      title: 'Archibald',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      title: 'Tome 1 archibald',
+      description: 'Livre policier en plusieurs tomes!',
+      imageUrl: "../../assets/images/book.jpg",
       createdDate: new Date(),
       snaps: 47,
       location: 'Paris'
     },
     {
       id: 2,
-      title: 'Three Rock Mountain',
-      description: 'Un endroit magnifique pour les randonnées.',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      title: 'Mountain',
+      description: 'livre de romance',
+      imageUrl: "../../assets/images/book-antique.jpg",
       createdDate: new Date(),
       snaps: 6,
       location: 'la montagne'
     },
     {
       id: 3,
-      title: 'Un bon repas',
-      description: 'Mmmh que c\'est bon !',
-      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      title: 'Jumanji',
+      description: 'livre de science fiction',
+      imageUrl: "../../assets/images/books.jpg",
       createdDate: new Date(),
       snaps: 156
     },
     {
       id: 4,
-      title: 'Archibald',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      title: 'Tome 2 archibald',
+      description: 'Livre policier en plusieurs tomes!',
+      imageUrl: "../../assets/images/book-library.jpg",
       createdDate: new Date(),
       snaps: 89,
       location: 'Paris'
     },
     {
       id: 5,
-      title: 'Three Rock Mountain',
-      description: 'Un endroit magnifique pour les randonnées.',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      title: 'Chair de poule',
+      description: 'Un livre fantastique ',
+      imageUrl: "../../assets/images/book-desk.jpg",
       createdDate: new Date(),
       snaps: 27,
       location: 'la montagne'
     },
     {
       id: 6,
-      title: 'Un bon repas',
-      description: 'Mmmh que c\'est bon !',
+      title: 'Tome 3 archibald',
+      description: 'Livre policier en plusieurs tomes!',
       imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
       createdDate: new Date(),
       snaps: 101

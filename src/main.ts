cc36832f328f151363f3b27b910532bc3fc333c8
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-04-09 11:23:29 +0200

    add folder environments and fonction enableProdMode() in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+//import de enableprodMode pour deployer l application en production
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));

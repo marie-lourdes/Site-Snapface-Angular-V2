b94bf6582c1203b6805c7a967064ec0629fcb639
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-04-09 11:52:04 +0200

    test observable rxjs, delete observable interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+//import des observable de la librairie rxjs pour la programmation reactive(avec les evenements en temps réel) et permet de composer du code asynchrone
+//import { interval, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  ngOnInit() {}
+  ngOnInit() {
+   
+  }
 }

f055dabc137e0cf4ee6768f7e8d272057da4a5f8
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-04-20 13:59:33 +0200

    create interval$  memory leak

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -17,11 +17,12 @@ export class FaceSnapListComponent implements OnInit {
 
   ngOnInit(): void {
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+
     // en changeant de page  et revenant sur la page la class FaceSnapListCompoment apelle a nouveau ngOnInit() et initialise a nouveau une autre instance de l observable interval$ cree avec subscribe
+    // par defaut l observable cree une nouvel instance avec subscribe()
     // a chaque fois qu on sort et revient sur la page d autres instance de l Observable est créé avec subscribe 
     //et les flux des precedentes instances de l Observable continue à émettre car nous avons pas unsubscribe ou limiter les emissions
     //ce qu on appelle une fuite de memoire, il faut mettre en place une strategie de unsubscribe
     const interval$ = interval(1000).subscribe(console.log)
   }
-
 }
